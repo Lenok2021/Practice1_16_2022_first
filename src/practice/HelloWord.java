ac785aac8da135bb74e93ad6edf6d67ddd444feb
cross <crosstown11223@yahoo.com>
2022-01-16 09:25:35 -0500

    created  return method sumNumbers

--- src/practice/HelloWord.java
+++ src/practice/HelloWord.java
@@ -3,5 +3,10 @@ package practice;
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("Hello word"); // this is a print statement
+
+
+    }
+    private static int sumNumbers(int a, int b){
+        return a+b;
     }
 }
