c804ba13751929310a8f974c7ae4c424e8cc4cf5
cross <crosstown11223@yahoo.com>
2022-01-16 09:26:55 -0500

    called method sumNumbers  and performed task

--- src/practice/HelloWord.java
+++ src/practice/HelloWord.java
@@ -3,8 +3,11 @@ package practice;
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("Hello word"); // this is a print statement
+        System.out.println("------------------------------------------");
 
-
+        int a=3;
+        int d=5;
+        int result = sumNumbers(a,d);
     }
     private static int sumNumbers(int a, int b){
         return a+b;
